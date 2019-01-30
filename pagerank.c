fde6faed4ee8e4be5969dc3bc8f8cd3b5383b91d
Utilisateur <user@debian.arcahe.ovh>
2019-01-30 15:29:05 +0100

    suite

--- pagerank.c
+++ pagerank.c
@@ -1,37 +1,36 @@
 #include <stdio.h>														
 #include <stdlib.h>	
 
-struct element
+typedef struct element
 {
 	double val;
 	int i;	//lignes
-	int j;	//colonnes															
-												
-};
-
-typedef struct element* tab;
+	int j;	//colonnes																											
+} ELEMENT;
 
 int main() {
+	printf("a");
 	int i,j; i = 0; j = 0;
 	int degre;	//degre sortant
 	int page;	//page courante
 	int m;		//nb liens
 	int nbp;	//nb pages
 	FILE *f= fopen("web1.txt", "r"); 		
-	tab t;
+	
 	fscanf(f, "%d\n", &nbp);
 	fscanf(f, "%d\n", &m);
-	t = malloc(m);
-	
+	ELEMENT *t = malloc(m*sizeof(ELEMENT));
+	printf("a");
 	for (i = 0; i < m; i++){
 		fscanf(f, "%d", NULL);
 		fscanf(f, "%d", &degre);
 		for (j = 0; j < degre; j++){
 			fscanf(f, "%d", &t[i].j);
 			fscanf(f, "%f", &t[i].val);
-			fprintf(f "%f", &t[i].val);
+			
 	}}
-	
+	free(t);
+	return 0;
 }		
 			
 			
