1c005be3f11071043019e54817c9b14050f79fd5
Maxime <maxime.mahout@ens.uvsq.fr>
2019-02-02 15:36:31 +0100

    ajouts mineurs

--- ranking.c
+++ ranking.c
@@ -23,7 +23,6 @@ TRIPLET* inserer_triplet(int pageC, int pageS, double proba, TRIPLET* next) {
 void inserer_liste(TRIPLET** st, int pageC, int pageS, double proba) {
 	TRIPLET *tmp  = *st;
 	*st = inserer_triplet(pageC, pageS, proba, tmp);
-	//printf("%p %d %d\n", (*st)->next, (*st)->i, (*st)->j);
 }
 
 void detruire_liste(TRIPLET* l) {
@@ -63,9 +62,9 @@ double difference_norme(double* p, double* old_p, int n) {
 }
 
 void power_method(double* p, TRIPLET** H, int n) {
-	int i, j, cpt;
+	int i, j;
 	TRIPLET *l;
-	double sum, *old_p;
+	double sum, beta, *old_p;
 	
 	// initialisation des tableaux de probas
 	old_p = calloc(n, sizeof(double));
@@ -75,40 +74,59 @@ void power_method(double* p, TRIPLET** H, int n) {
 	}
 
 	// power method
-	cpt = 0;
-	while (difference_norme(p, old_p, n) > 10E-12) {
+	while (difference_norme(p, old_p, n) > 10E-12) 
+	{
 		memcpy(old_p, p, n*sizeof(double));
-		
+		beta = 0;
 		for (i = 0; i < n; i++) // p(i) = somme p(j) H(j, i)
 		{
 			sum = 0;
 			l = H[i]; // colonne i
 			
-			while (l != NULL) { // pour tous H(j, i) != 0
+			if (l == NULL) beta += old_p[i-1]; // calcul beta
+			
+			while (l != NULL)  // pour tous H(j, i) != 0 
+			{
 				j = l->i; // ligne j
 				sum += old_p[j-1] * l->proba; // produit matriciel
 				l = l->next;
 			}
 			
 			p[i] = sum; // p = p*H
-			// p[i] = alpha * sum + (1-alpha) / n; // calcul de p*G = f(p*H)
 		}
-		cpt++;
+		for (i = 0; i < n; i++) // calcul de p = p*G = f(p*H) 
+		{ 
+			p[i] = alpha * p[i] + (1 - alpha) / n + (alpha * beta) / n; 
+		}
 	}	
-	
 	free(old_p);
 }
 
+void afficher_triplets(TRIPLET** st, int n) {
+	int j;
+	TRIPLET *l;
+	for (j = 0; j < n; j++)
+	{
+		l = st[j];
+		
+		while (l != NULL) {
+			printf("%d\t%d\t%lf\n", l->j, l->i, l->proba);
+			l = l->next;
+		}
+	}	
+}
+
 int main(int argc, char** argv) {
-	int i, j, k, n, m;
+	int i, j, k; 
+	int n, m;
 	
     // lecture du ficher
     FILE *web = fopen("web1.txt","r");
     fscanf(web, "%d\n", &n);
     fscanf(web, "%d\n", &m);
    
-	printf("%d\n", n);
-	printf("%d\n", m);
+	printf("%d pages\n", n);
+	printf("%d liens\n", m);
 	
 	// allocation du tableau de pointeurs de triplets
 	TRIPLET **st = calloc(n, sizeof(TRIPLET*));
@@ -137,28 +155,19 @@ int main(int argc, char** argv) {
 		
 		fscanf(web, "\n");
 	}
-	
-	// affichage temporaire
-	TRIPLET *l;
-	for (j = 0; j < n; j++)
-	{
-		l = st[j];
-		
-		while (l != NULL) {
-			printf("%d\t%d\t%lf\n", l->j, l->i, l->proba);
-			l = l->next;
-		}
-	}
 	fclose(web);
+	
+	//~ afficher_triplets(st, n);
 	
 	power_method(p, st, n);
 	
-	// affichage du tableau de probas après power method
+	// ecriture du tableau de probas après power method
+	FILE *out = stdout; // fopen("output.txt","w");
 	for (i = 0; i < n; i++)
 	{
-		printf("Page %d %lf\n", i+1 , p[i]);		
+		fprintf(out, "Page %d %lf\n", i+1 , p[i]);		
 	}	
-	printf("Somme probas : %lf\n", somme(p, n));	
+	fprintf(out, "Somme probas : %lf\n", somme(p, n));	
 	
 	detruire_tableau_listes(st, n);
 	free(p);	
