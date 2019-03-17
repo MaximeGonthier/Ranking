d969d4fe9057c119321b534077c3f25ad6cccec2
Utilisateur <user@debian.arcahe.ovh>
2019-03-17 16:21:55 +0100

    anneau

--- ajoutsommetsattaquantz.c
+++ ajoutsommetsattaquantz.c
@@ -7,6 +7,7 @@
 //forte : Page 280545 9.96199e-05
 //moyenne : Page 281466 7.53954e-06
 //faible : Page 281574 6.05222e-07
+//Staford de base : 281903 2312497
 			
 void ajoutsommetseul (int nbajout, char* nom, int nbpages, int nbliens, int perticible) {
 
@@ -24,6 +25,26 @@ void ajoutsommetseul (int nbajout, char* nom, int nbpages, int nbliens, int pert
 	
 	FILE *h = fopen(nom,"r+");
 	fprintf(h, "%d %d", nbpages+nbajout, nbliens+nbajout);
+	fclose(h);
+}
+
+void ajoutanneau (int nbajout, char* nom, int nbpages, int nbliens, int perticible) {
+
+	int degre = 1;
+	int i;
+	int cible = 0;
+	if (perticible == 1) { cible = 280545; }
+	if (perticible == 2) { cible = 281466; }
+	if (perticible == 3) { cible = 281574; }
+	FILE *g = fopen(nom,"a");
+	for (i = 1; i < nbajout; i++) {
+		fprintf(g, "%d %d %d 1.000000\n", nbpages+i, degre, nbpages+i+1);
+	}
+		fprintf(g, "%d %d %d 0.500000 %d 0.500000\n", nbpages+i, 2, nbpages+1, cible);
+	fclose(g);
+	
+	FILE *h = fopen(nom,"r+");
+	fprintf(h, "%d %d", nbpages+nbajout, nbliens+nbajout+1);
 	fclose(h);
 }
 
@@ -57,7 +78,10 @@ int main(int argc, char** argv) {
 
 	
 
-	if (structure = 1) { ajoutsommetseul(nbajout,argv[1], nbpages, nbliens, cible); }
+	if (structure == 1) { ajoutsommetseul(nbajout,argv[1], nbpages, nbliens, cible); }
+	else
+	if (structure == 2) { ajoutanneau(nbajout,argv[1], nbpages, nbliens, cible); }
+	
 
 	return 0;
 }
