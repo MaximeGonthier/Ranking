efa437fe436b35b5ab2db7e289eab973b69baa79
Utilisateur <user@debian.arcahe.ovh>
2019-04-05 15:26:41 +0200

    gf

--- R/Cible_perti_faible_graphe_alea/Draw.R
+++ R/Cible_perti_faible_graphe_alea/Draw.R
@@ -16,5 +16,10 @@ qqnorm(Anneau_aleatoire[,1], col="blue")
 hist(Anneau_aleatoire[,1], col = "lightblue", prob=T)
 den <- density(Anneau_aleatoire[,1])
 lines(den, col = "red")
-abline(v = moy, col = "black", lty ="dotted")
-curve(dnorm(x, moy, ecart), add=T, col="blue")
+#~ abline(v = moy, col = "black", lty ="dotted")
+#~ curve(dnorm(x, moy, ecart), add=T, col="blue")
+
+#~ pourcentage de valeur en 8 et 6
+pnorm(lower.tail = 6e-06, upper.tail = 8e-06)
+pnorm(6e-06)
+#~ dnorm(c(5.0e-6,8.0e-6), 6.4284e-06,1.4494e-06)
